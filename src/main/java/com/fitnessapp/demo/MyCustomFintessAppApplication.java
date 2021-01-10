b8900735109cd845410e559d9bcf15a515e0e922
HristoGetov <260064@via.dk>
2021-01-10 18:30:32 +0200

    create a new project

---
+++ src/main/java/com/fitnessapp/demo/MyCustomFintessAppApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCustomFintessAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCustomFintessAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitnessapp/demo/MyCustomFintessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCustomFintessAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
